1276cde82bffa71bd4e4051fa91c92b9129be0bb
3ef1rus <10kmmrza10k@mail.ru>
2022-09-29 15:02:12 +0300

    first commit

---
+++ lab2.js
@@ -0,0 +1,23 @@
+let calculator = {
+  first: null,
+  second: null,
+  read() {
+    this.first = prompt("Введите первое значение")
+    this.second = prompt("Введите второе значение")
+  },
+  sum() {
+    return this.first + this.second;
+  },
+  mul() {
+    return this.first * this.second;
+  }
+}
+function Accumulator(startingValue){
+  let object={
+    value:startingValue,
+    read(){
+      let a = prompt('Какое число хотите прибавить ?');
+      this.value=this.value+a;
+    }
+  }
+}
